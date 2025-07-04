63adb240cfebc004d873b3d9f3bf780c68748d23
Kamil <kamilgolub0@gmail.com>
2025-07-04 18:29:02 +0200

    Revert "Back to cp"
    This reverts commit 9535bd9ccbb450a29ed1bd56456798e8bde24d52.

--- src/config.sh
+++ src/config.sh
@@ -6,14 +6,16 @@ function configure_desktop() {
     [ ! -d $HOME/.local/bin ] && mkdir -p $HOME/.local/bin
 
     for dir in config/{polybar,xmonad,rofi,kitty,dunst,picom,fastfetch}; do
-        cp -r "$dir" "$HOME/.config/"
+        ln -s "$(pwd)/$dir" "$HOME/.config/"
     done
-    
-    cp ./config/bash/bashrc $HOME/.bashrc
-    cp -r ./config/fastfetch $HOME/.config/
-    cp ./config/starship/starship.toml $HOME/.config/starship.toml
-    cp ./scripts/* $HOME/.local/bin
+
+    ln -s "$(pwd)/config/bash/bashrc" $HOME/.bashrc
+    ln -s "$(pwd)/config/starship/starship.toml" $HOME/.config/starship.toml
     sudo cp -r ./config/plymouth /usr/share/plymouth/themes/limos
+
+    for script in config/*; do
+        ln -s "$(pwd)/$script" $HOME/.local/bin/
+    done
 
     [ ! -d $HOME/.icons ] && mkdir $HOME/.icons
     [ ! -d $HOME/.themes ] && mkdir $HOME/.themes
